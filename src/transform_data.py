9311db15aa8500b0340e46f93d04811fb28da4eb
Ranko Liang <rankoliang@gmail.com>
2021-01-30 12:34:55 -0500

    Update functions to be case insensitive

--- src/transform_data.py
+++ src/transform_data.py
@@ -4,8 +4,8 @@ from pyspark.sql.functions import lower
 
 
 def good_standing(df: DataFrame) -> DataFrame:
-    return df.filter(df.loan_status != "Charged Off")
+    return df.filter(lower(df.loan_status) != "charged off")
 
 
 def known_purpose(df: DataFrame) -> DataFrame:
-    return df.filter(df["purpose"] != "other")
+    return df.filter(lower(df.purpose) != "other")
--- tests/test_transform_data.py
+++ tests/test_transform_data.py
@@ -11,32 +11,52 @@ def spark():
 
 
 class TestGoodStanding:
-    @pytest.fixture
-    def data(self):
-        return [(0, "Charged Off"), (1, "Fully Paid")]
+    @pytest.fixture(scope="class")
+    def schema(self):
+        return ["id", "loan_status"]
 
     @pytest.fixture
-    def df(self, spark, data):
-        return spark.createDataFrame(data, ["id", "loan_status"])
+    def data(self):
+        return [(0, "Charged Off"), (1, "Fully Paid"), (2, "charged off")]
+
+    @pytest.fixture
+    def df(self, spark, data, schema):
+        return spark.createDataFrame(data, schema)
 
     def test_filters_charged_off(self, df):
         assert good_standing(df).filter(df["loan_status"] == "Charged Off").count() == 0
 
-    def test_only_filters_charged_off(self, df):
-        assert good_standing(df).count() == 1
+    def test_only_filters_charged_off(self, df, spark, schema):
+        assert (
+            good_standing(df).collect()
+            == spark.createDataFrame([(1, "Fully Paid")], schema).collect()
+        )
+
+    def test_filters_charged_off_case_insensitive(self, df):
+        assert good_standing(df).filter(df["loan_status"] == "charged off").count() == 0
 
 
 class TestKnownPurpose:
+    @pytest.fixture(scope="class")
+    def schema(self):
+        return ["id", "purpose"]
+
     @pytest.fixture
     def data(self):
-        return [(0, "mortgage"), (1, "other")]
+        return [(0, "mortgage"), (1, "other"), (2, "Other")]
 
     @pytest.fixture
-    def df(self, spark, data):
-        return spark.createDataFrame(data, ["id", "purpose"])
+    def df(self, spark, data, schema):
+        return spark.createDataFrame(data, schema)
 
-    def test_filters_charged_off(self, df):
+    def test_filters_other(self, df):
         assert known_purpose(df).filter(df["purpose"] == "other").count() == 0
 
-    def test_only_filters_charged_off(self, df):
-        assert known_purpose(df).count() == 1
+    def test_filters_other_case_insensitive(self, df):
+        assert known_purpose(df).filter(df["purpose"] == "other").count() == 0
+
+    def test_only_filters_other(self, df, spark, schema):
+        assert (
+            known_purpose(df).collect()
+            == spark.createDataFrame([(0, "mortgage")], schema).collect()
+        )
